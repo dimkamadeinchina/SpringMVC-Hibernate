968e922adaf7490921f1482a01f674c345e87ab1
dimkamadeinchina <webstrem007@mail.ru>
2017-07-31 19:12:32 +0800

    simple CRUD

--- SimpleCRUDApp/src/main/java/com/example/controller/CompanyController.java
+++ SimpleCRUDApp/src/main/java/com/example/controller/CompanyController.java
@@ -4,7 +4,9 @@ import com.example.model.Company;
 import com.example.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,5 +22,32 @@ public class CompanyController {
     public ModelAndView getAllEmployees() {
         List<Company> companyList = companyService.getAllCompanies();
         return new ModelAndView("companies", "companyList", companyList);
+    }
+
+    @RequestMapping("createCompany")
+    public ModelAndView createCompany(@ModelAttribute Company company) {
+        return new ModelAndView("company-form");
+    }
+
+    @RequestMapping("editCompany")
+    public ModelAndView editCompany(@RequestParam long id, @ModelAttribute Company company) {
+        company = companyService.getCompany(id);
+        return new ModelAndView("company-form", "companyObject", company);
+    }
+
+    @RequestMapping("saveCompany")
+    public ModelAndView saveCompany(@ModelAttribute Company company) {
+        if(company.getId() == 0){
+            companyService.createCompany(company);
+        } else {
+            companyService.updateCompany(company);
+        }
+        return new ModelAndView("redirect:getAllCompanies");
+    }
+
+    @RequestMapping("deleteCompany")
+    public ModelAndView deleteCompany(@RequestParam long id) {
+            companyService.deleteCompany(id);
+        return new ModelAndView("redirect:getAllCompanies");
     }
 }
--- SimpleCRUDApp/src/main/webapp/WEB-INF/views/companies.jsp
+++ SimpleCRUDApp/src/main/webapp/WEB-INF/views/companies.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: ДИМА
-  Date: 30.07.2017
-  Time: 20:28
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
@@ -12,7 +5,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Employees List</title>
+    <title>Companies List</title>
     <!-- Bootstrap CSS -->
     <%-- <link href="<c:url value="/resources/css/bootstrap.min.css" />" rel="stylesheet"> --%>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
